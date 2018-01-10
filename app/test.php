aad6c3f02fd8a2f8ca5e3d89561afe535256210c
opspy <houshiying@vip.qq.com>
2018-01-10 10:22:14 +0800

    Update test.php

--- app/test.php
+++ app/test.php
@@ -1,13 +1,13 @@
 <?php 
    $link=mysql_connect("mysql","root","123456"); 
    if(!$link) echo "MYSQL FAILD!连接错误，用户名密码不对"; 
-   else echo "MYSQL OK!可以连接"; 
+   else echo "MYSQL OK!可以连接 <br/>"; 
 
    $redis = new Redis();    
    $redis->connect("redis","6379");  //php客户端设置的ip及端口    
    //存储一个 值    
    $redis->set("key1","Hello redis!");    
-   echo $redis->get("key1");     //应输出hello world  
+   echo $redis->get("key1")"<br/>";     //应输出hello world  
   
    //1. 创建对象 
    $mem = new Memcache();
@@ -15,11 +15,11 @@
    $mem->addServer("memcached",11211);
    //3. 放置信息 
    $mem->add("mystr","hello memcache!",MEMCACHE_COMPRESSED,0); 
-   $mem->add("myarray",array(10,20,30,40),MEMCACHE_COMPRESSED,0); 
+   //$mem->add("myarray",array(10,20,30,40),MEMCACHE_COMPRESSED,0); 
    #$mem->add("myob",new Stu(),MEMCACHE_COMPRESSED,0);   #这里没给对象，字节写个试试
    //4. 获取信息
    echo $mem->get("mystr")."<br/>";
-   var_dump($mem->get('myarray')); 
+   //var_dump($mem->get('myarray')); 
    echo "<br/>"; 
    #$mem->get("myob")->getinfo();
       
