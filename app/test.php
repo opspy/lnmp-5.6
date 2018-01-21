3811b69e4f2317521a054fb5c2f7d2dbb2050e11
opspy <houshiying@vip.qq.com>
2018-01-21 20:38:00 +0800

    update settings

--- app/test.php
+++ app/test.php
@@ -1,15 +1,15 @@
 <?php 
-   $link=mysql_connect("mysql","root","123456"); 
-   if(!$link) echo "MYSQL FAILD!连接错误，用户名密码不对"; 
-   else echo "MYSQL OK!可以连接 <br/>"; 
+   $mysql=mysql_connect("mysql","root","123456"); 
+   if(!$mysql) echo "MySQL connect failed!"; 
+   else echo "Hello MySQL!<br/>"; 
 
    $redis = new Redis();    
    $redis->connect("redis","6379");
-   $redis->set("key1","Hello redis!");    
+   $redis->set("key1","Hello Redis!");    
    echo $redis->get("key1")."<br/>";
    
    $mem = new Memcache();
    $mem->addServer("memcached",11211);
-   $mem->add("mystr","hello memcached!",MEMCACHE_COMPRESSED,0); 
-   echo $mem->get("mystr")."<br/>";
+   $mem->add("key1","hello Memcached!",MEMCACHE_COMPRESSED,0); 
+   echo $mem->get("key1")."<br/>";
 ?> 
